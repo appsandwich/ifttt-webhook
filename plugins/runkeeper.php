d0838388f3cf1ccda81d3b8a6cd10c016a6945ba
Vinny Coyne <info@appsandwich.com>
2013-07-22 12:41:45 +0100

    runkeeper plugin

---
+++ plugins/runkeeper.php
@@ -0,0 +1,62 @@
+<?php
+
+    /**
+     * Runkeeper plugin.
+     * 
+     * This plugin takes the content of the body and assumes that it is valid json, decodes it and replaces 
+     * any of the original variables passed.
+     * 
+     * This lets you mimic payloads expected by various webhook endpoints.
+     */
+    class Runkeeper extends Plugin {
+        
+        public function execute($plugin, $object, $raw) {
+            
+            __log("Raw JSON string passed: '{$object->description}'");
+            
+            $json = json_decode($object->description);
+            if (!$json) {
+                __log("Invalid JSON payload '$json'", 'ERROR');
+                return false;
+            }
+            
+            // Convert some of valuse from seconds to minutes (Runkeeper uses minutes, weirdly enough)
+            $json['total_sleep'] = $json['total_sleep'] / 60.0
+            $json['deep'] = $json['deep'] / 60.0
+            $json['light'] = $json['light'] / 60.0
+            $json['awake'] = $json['awake'] / 60.0
+            
+            // FellAsleepAt is formatted: August 23, 2013 at 11:01PM
+            // Convert to Runkeeper's preferred format: Sat, 1 Jan 2011 00:00:00
+            // timestamp_datetime = DateTime.parse(timestamp)
+            // formatted_timestamp = timestamp_datetime.strftime("%a, %d %b %Y %H:%M:%S")
+            $rk_timestamp = date("%D, %j %M %Y %H:%i:%s", strptime($json['timestamp'], "%F %d, %Y at %H:%i%A"));
+            __log("Converted timestamp: $rk_timestamp");
+            $json['timestamp'] = $rk_timestamp
+            
+            $json_string = json_encode($json);
+            
+            $ch = curl_init('https://api.runkeeper.com/sleep');
+            
+            //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            
+            // Verify that we're actually connecting to the Runkeeper API
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+            curl_setopt($ch, CURLOPT_CAINFO, getcwd() . "/certs/api-runkeeper-com.crt");
+            
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $json_string);                                                                  
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+                'Content-Type: application/vnd.com.runkeeper.NewSleep+json',                                                                                
+                'Content-Length: ' . strlen($json_string)),
+                'Authorization: Bearer YOUR_ACCESS_TOKEN'
+            );                                                                                                                   
+            
+            $result = curl_exec($ch);
+            
+            return $json;
+            
+        }
+    }
